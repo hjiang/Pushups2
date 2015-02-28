f8655f00060d5178a338fc7fb266996b64cb072b
Hong Jiang <hjiang@avos.com>
2015-02-28 13:03:17 +0800

    Test the proximity device API.

--- public/js/pushups.js
+++ public/js/pushups.js
@@ -1,1 +1,14 @@
-console.log('Test');
+$(function() {
+  window.addEventListener('deviceproximity', function(event) {
+    // The maximum distance the sensor covers (in cm).
+    var max = event.max;
+    // The minimum distance the sensor covers (in cm).
+    var min = event.min;
+    // The device proximity (in cm).
+    var proximity = event.value;
+
+    $('#max').text(max);
+    $('#min').text(min);
+    $('#current').text(proximity);
+  });
+});
